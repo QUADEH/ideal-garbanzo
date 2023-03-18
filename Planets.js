61dd16e6aa3dc7988e3a0daa422afb0492e7c64d
QUADEH <127901701+QUADEH@users.noreply.github.com>
2023-03-18 10:50:25 -0500

    Update Planets.js

--- Planets.js
+++ Planets.js
@@ -13,3 +13,22 @@ let buttonCounts = {
   Uranus: 0,
   Neptune: 0
 };
+
+function resetCounts() {
+  buttonCounts.Mercury = 0;
+  buttonCounts.Venus = 0;
+  buttonCounts.Earth = 0;
+  buttonCounts.Mars = 0;
+  buttonCounts.Saturn = 0;
+  buttonCounts.Jupiter = 0;
+  buttonCounts.Uranus = 0;
+  buttonCounts.Neptune = 0;
+  document.getElementById("Mercury-text").innerHTML = "";
+  document.getElementById("Venus-text").innerHTML = "";
+  document.getElementById("Earth-text").innerHTML = "";
+  document.getElementById("Mar-text").innerHTML = "";
+  document.getElementById("Saturn-text").innerHTML = "";
+  document.getElementById("Jupiter-text").innerHTML = "";
+  document.getElementById("Uranus-text").innerHTML = "";
+  document.getElementById("Neptune-text").innerHTML = "";
+}
